2ff67797536f388bd4326db1d4b5f0c1648f1e0c
sopigoo <shofinadhifa24@gmail.com>
2024-09-10 07:12:14 +0700

    Django project setup

---
+++ grab_it_now/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for grab_it_now project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grab_it_now.settings')
+
+application = get_asgi_application()
