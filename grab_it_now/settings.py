a41f46980fdb437bf3c6dfc2be11d941a7bcacaf
sopigoo <shofinadhifa24@gmail.com>
2024-09-18 10:59:08 +0700

    Add auto push PWS

--- grab_it_now/settings.py
+++ grab_it_now/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://siti-shofi-grabitnow.pbp.cs.ui.ac.id", "https://siti-shofi-grabitnow.pbp.cs.ui.ac.id"]
