0a4ca71d99f1d54ca49e6c90ef393e223bd9f84d
sopigoo <shofinadhifa24@gmail.com>
2024-09-24 01:05:00 +0700

    Finished assignment 4

--- grab_it_now/settings.py
+++ grab_it_now/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y&_ck7!p!e^*!j7ebdqe#n#022csh$$2$-^8u_m-k@*$xa6zc*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "siti-shofi-grabitnow.pbp.cs.ui.ac.id"]
 
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from main.views import show_main, add_product, show_xml, show_json, show_xml_by_id, show_json_by_id, delete_product
+from main.views import register, login_user, logout_user
 
 app_name = 'main'
 
@@ -11,4 +12,7 @@ urlpatterns = [
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('delete-product/<uuid:id>/', delete_product, name='delete_product'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
